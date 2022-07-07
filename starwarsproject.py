604f3d720e8f0fe9446e197d4686ff7fb2ad3917
wmully <wmully@gmail.com>
2022-07-07 23:22:56 +0100

    Working version. Tested and seen starships correct in mongo

--- starwarsproject.py
+++ starwarsproject.py
@@ -40,12 +40,45 @@ pilot_dict={}
 
 ## Next to get all the pilots names by iterating over all the pilots in the ships list
 for s in ships:
-    if s["pilots"]:
-        for p in s["pilots"]:
+    if s.get("pilots"):
+        for p in s.get("pilots"):
             if p not in pilot_dict:   ### If this pilot has not already been fetched then go call the api
                 name=get_pilot_name(p)
-                pilot_dict[name]=p
+                pilot_dict[p]=name
 
 print(pilot_dict)
 
 ## Now go search for each pilot in mongo and get the ObjectIDs
+client = pymongo.MongoClient()
+db = client['starwars']
+
+## Use another key-value dictionary gfor looking up the name to the objectid
+pilotobj_dict={}
+
+for pname in pilot_dict.values():
+    pdoc=db.characters.find_one({"name": pname})
+    if pname not in pilotobj_dict:
+        pilotobj_dict[pname]=pdoc.get("_id")
+
+print ("--- PILOTOBJ DICT---")
+print (pilotobj_dict)
+
+### Now we overwrite the ships list so that each ship's pilot array has the object ids.
+for s in ships:
+    new_pilot_list=[]
+    for p in s["pilots"]:
+        pname=pilot_dict.get(p)
+        objid=pilotobj_dict.get(pname)
+        new_pilot_list.append(objid)
+    s["pilots"] = new_pilot_list
+
+print ("--- SHIPS LIST---")
+pp.pprint(ships)
+
+### Now write the newly-updated starships collection into mongo
+db.starships.insert_many(ships)
+
+print ("--- SAVED TO MONGO ---")
+
+### Close the mongo connection
+client.close()
